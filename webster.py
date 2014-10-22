354734373bc7c0bae31b45765b82f9ffd74490c6
toost <toost>
2014-10-21 21:50:51 -0700

    Improved HTTP parsing, threads

--- webster.py
+++ webster.py
@@ -22,50 +22,38 @@ HOST = ''
 PORT = 6543
 BACKLOG = 5
 BUF_SIZE = 500
-MAX_THREADS = 50
+
+CODES = { 200:'200 OK', 400:'400 Bad Request', 404:'404 Not Found', 501:'501 Not Implemented' }
 
 
 def parseHTTP(d):
     """  """
     result = { 'comm':None, 'host':None, 'page':None, 'vsn':None }
-    ds = d.decode().split('\r\n')
+    ds = d.split('\r\n')
     count = 0
     for l in ds:
-        a = l.split()
-        if count == 0 and len(a) == 3:
-            result['comm'] = a[0]
-            result['page'] = a[1]
-            result['vsn'] = a[2].rstrip()
-        elif len(a) == 2:
-            result[a[0].lower()] = a[1].rstrip()
+        a = l.partition(':')
+        if count == 0 and len(a[0]) > 1:
+            x = a[0].split()
+            if len(x) == 3:
+                result['comm'] = x[0]
+                result['page'] = x[1]
+                result['vsn'] = x[2].rstrip()
+        elif len(a[0]) >= 1:
+            result[a[0].lower()] = a[2].rstrip()
         else:
             break
         count += 1
     return result
 
 
-def sendError(e):
-    """
-    Sends the appropriate error response.
-    Currently ONLY errors are returned by this server.
-    """
-    if e == 400:
-        msg = str(e) + ' ' + 'Bad Request'
-    elif e == 404:
-        msg = str(e) + ' ' + 'Not Found'
-    else:
-        e = 501
-        msg = '501 Not Implemented'
-    return header(msg,msg,'close')
-
-
-def header(msg,code,conn):
-    head = 'HTTP/1.1 ' + code + '\r\n'
+def htmlResponse(msg,code,conn):
     html = '<!DOCTYPE HTML><html><head><title>' + code
     html += '</title></head><body><h1>'+ msg + '</h1></body>'
     html = html.encode()
-    head += 'Content-Length: ' + str(len(html))
-    head += '\r\nConnection: ' + conn + '\r\nContent-type: text/html\r\n\r\n'
+    head = 'HTTP/1.1 ' + code + '\r\n'
+    head += 'Content-Length: ' + str(len(html)) + '\r\n'
+    head += 'Connection: ' + conn + '\r\nContent-type: text/html\r\n\r\n'
     head = head.encode()
     head += html
     print(head)
@@ -75,51 +63,47 @@ def header(msg,code,conn):
 def parseRequest(conn):
     """  """
     while True:
-        data = conn.recv(BUF_SIZE)
-        if data:
-            req = parseHTTP(data)
-            print(req)
-            if not req['comm']:
-                conn.sendall(sendError(400))
-                conn.close()
-                break
-            elif req['comm'] != 'GET':
-                conn.sendall(sendError(501))
-                conn.close()
-                break
-            elif req['comm'] == 'GET':
-                #conn.sendall(sendError(404))
-                msg = 'Hoi! Hoe gaat het,'+req['page']+'?  Gaat, dank je!'
-                conn.sendall(header(msg,'200 OK','keep-alive'))
-        else:
-            break
-    conn.close()
+        buff = ''
+        while '\r\n\r\n' not in buff:
+            data = conn.recv(BUF_SIZE).decode()
+            if not data:
+                return
+            buff += data
+        req = parseHTTP(buff)
+        print(req)
+        if not req['comm']:
+            conn.sendall(htmlResponse(CODES[400],CODES[400],'close'))
+            return
+        elif req['comm'] != 'GET':
+            conn.sendall(htmlResponse(CODES[501],CODES[501],'close'))
+            return
+        elif req['comm'] == 'GET':
+            msg = 'Hoi! Hoe gaat het, '+req['page']+'?  Gaat, dank je!'
+            conn.sendall(htmlResponse(msg,CODES[200],'keep-alive'))
 
 
-class MyThread(threading.Thread):
+class ClientThread(threading.Thread):
     def __init__(self,conn):
         threading.Thread.__init__(self)
         self.conn = conn
 
     def run(self):
-        print('Starting thread with conn:',self.conn)
         parseRequest(self.conn)
-        print('Closing thread with conn:',self.conn)
+        self.conn.close()
 
 
 def listenAndServe():
     """ Main HTTP listen & serve loop.  """
-    tpool = threading.BoundedSemaphore(value=MAX_THREADS)
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.bind((HOST,PORT))
+    s.listen(BACKLOG)
     while True:
         print("Threads:",threading.active_count())
-        #with tpool:
-        s.listen(BACKLOG)
         conn, addr = s.accept()
         print('Connected by', addr)
-        t = MyThread(conn)
+        t = ClientThread(conn)
         t.start()
+
 
 if __name__ == '__main__':
     listenAndServe()
